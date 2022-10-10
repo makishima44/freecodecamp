1d879067047ad3ee6920c1d445c59d3a19954f8c
Artur <metallist15431543@gmail.com>
2022-10-10 18:58:13 +0300

    Understand Functional Programming Terminology

---
+++ 64.js
@@ -0,0 +1,19 @@
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27)
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13)
